d5d3021d51b0453c45845eac01ac5e18cd30ada2
Mouhamadou <mouhamadoudioufsn@gmail.com>
2021-03-01 18:47:42 +0000

    Install react-router-dom

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAn3v_YXWlg-ttCUPkQ1J7gB-J4vD6h2nE',
+  authDomain: 'slack-clone-ab5f2.firebaseapp.com',
+  projectId: 'slack-clone-ab5f2',
+  storageBucket: 'slack-clone-ab5f2.appspot.com',
+  messagingSenderId: '930232536225',
+  appId: '1:930232536225:web:aef389be655f10a197b4e0',
+};
