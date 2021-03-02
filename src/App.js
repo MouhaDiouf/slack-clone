2b293310c2ec6d2edf6eb6f81a070f0dd3764c48
Mouhamadou <mouhamadoudioufsn@gmail.com>
2021-03-02 19:38:22 +0000

    Create the Sidebar component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import { Counter } from './features/counterSlice';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from './Header';
+import styled from 'styled-components';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <>
-          {/* A <Switch> looks through its children <Route>s and
+        <Header />
+        {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
+        <AppBody>
+          <Sidebar />
           <Switch>
             <Route exact path="/">
-              <Header />
+              {/* Chat */}
             </Route>
           </Switch>
-        </>
+        </AppBody>
       </Router>
     </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
---
+++ src/Sidebar.js
@@ -0,0 +1,63 @@
+import { Create, FiberManualRecord } from '@material-ui/icons';
+import React from 'react';
+import styled from 'styled-components';
+
+function Sidebar() {
+  return (
+    <SidebarContainer>
+      <SidebarHeader>
+        <SidebarInfo>
+          <h2>Papa Fam HQ</h2>
+          <h3>
+            <FiberManualRecord />
+            Mouhamadou
+          </h3>
+        </SidebarInfo>
+        <Create />
+      </SidebarHeader>
+    </SidebarContainer>
+  );
+}
+
+const SidebarContainer = styled.div`
+  color: white;
+  background-color: var(--slack-color);
+  flex: 0.3;
+  margin-top: 60px;
+`;
+const SidebarHeader = styled.div`
+  display: flex;
+  border-bottom: 1px solid #49274b;
+  padding: 13px;
+
+  > .MuiSvgIcon-root {
+    padding: 8px;
+    color: #49274b;
+    font-size: 18px;
+    background-color: white;
+    border-radius: 999px;
+  }
+`;
+export default Sidebar;
+const SidebarInfo = styled.div`
+  flex: 1;
+  > h2 {
+    font-size: 15px;
+    font-weight: 900;
+    margin-bottom: 5px;
+  }
+
+  > h3 {
+    display: flex;
+    font-size: 13px;
+    font-weight: 400;
+    align-items: center;
+
+    > .MuiSvgIcon-root {
+      font-size: 14px;
+      margin-top: 1px;
+      margin-right: 2px;
+      color: green;
+    }
+  }
+`;
