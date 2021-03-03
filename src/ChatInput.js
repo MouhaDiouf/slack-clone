9241c6eafc99ece676d1ec2743f161c95ca51fa5
MouhaDiouf <mouhamadoudioufsn@gmail.com>
2021-03-03 17:52:21 +0000

    Create the chatinput container

---
+++ src/ChatInput.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function ChatInput() {
+   return (
+      <ChatInputContainer>
+         <form >
+            <input type="text"/>
+         </form>
+      </ChatInputContainer>
+   )
+}
+
+export default ChatInput
+
+
+const ChatInputContainer = styled.div``
